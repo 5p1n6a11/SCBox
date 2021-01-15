190f879950cba2ce48f04ac6978f196b44e09677
u2i <pv3f3wii@s.okayama-u.ac.jp>
2021-01-15 21:47:24 +0900

    fix

--- hook/syscall_hook.c
+++ hook/syscall_hook.c
@@ -3,13 +3,13 @@
 #include <linux/unistd.h>
 #include <linux/utsname.h>
 #include <asm/pgtable.h>
-#include <linux/kallsyms.h>
+#include <linux/kprobes.h>
 
 MODULE_DESCRIPTION("system call replace test module");
 MODULE_AUTHOR("u2i");
 MODULE_LICENSE("GPL");
 
-static void **sys_call_table;
+static void **syscall_table;
 
 asmlinkage long (*orig_sys_uname)(struct old_utsname __user *buf);
 
@@ -21,7 +21,7 @@ asmlinkage long syscall_replace_sys_uname(struct old_utsname __user *buf)
 
 static void save_original_syscall_address(void)
 {
-    orig_sys_uname = sys_call_table[__NR_uname];
+    orig_sys_uname = syscall_table[__NR_uname];
 }
 
 static void change_page_attr_to_rw(pte_t *pte)
@@ -39,19 +39,25 @@ static void replace_system_call(void *new)
     unsigned int level = 0;
     pte_t *pte;
 
-    pte = lookup_address((unsigned long) sys_call_table, &level);
-    /* Need to set r/w to a page which sys_call_table is in. */
+    pte = lookup_address((unsigned long) syscall_table, &level);
+    /* Need to set r/w to a page which syscall_table is in. */
     change_page_attr_to_rw(pte);
 
-    sys_call_table[__NR_uname] = new;
+    syscall_table[__NR_uname] = new;
     /* set back to read only */
     change_page_attr_to_ro(pte);
 }
 
+static struct kprobe kp = {
+    .symbol_name = "sys_call_table"
+};
+
 static int syscall_replace_init(void)
 {
-    sys_call_table = (void *) kallsyms_lookup_name("sys_call_table");
-    pr_info("sys_call_table address is 0x%p\n", sys_call_table);
+    register_kprobe(&kp);
+    syscall_table = (void *) kp.addr;
+    unregister_kprobe(&kp);
+    pr_info("sys_call_table address is 0x%p\n", syscall_table);
 
     save_original_syscall_address();
     pr_info("original sys_uname's address is 0x%p\n", orig_sys_uname);
